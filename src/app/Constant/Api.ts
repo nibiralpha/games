c441fcff4c3b6a5ea542cb7ba71fc4f9683efc23
Nibir Alam <nibiralpha@gmail.com>
2026-09-01 20:18:06 +0600

    live backed

--- src/app/Constant/Api.ts
+++ src/app/Constant/Api.ts
@@ -1,1 +1,2 @@
-export const BASEURL: string = `http://localhost:5001`;
+// export const BASEURL: string = `http://localhost:5001`;
+export const BASEURL: string = `https://games-backend-yndp.onrender.com`;
