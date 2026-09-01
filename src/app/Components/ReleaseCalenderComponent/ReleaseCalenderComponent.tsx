2ec562415bd3f67a551fd3a1b613d09a5c9016c5
Nibir Alam <nibiralpha@gmail.com>
2026-09-01 20:40:19 +0600

    fix cursor pointer issue

--- src/app/Components/ReleaseCalenderComponent/ReleaseCalenderComponent.tsx
+++ src/app/Components/ReleaseCalenderComponent/ReleaseCalenderComponent.tsx
@@ -28,7 +28,7 @@ export default function ReleaseCalenderComponent({
           <p className="text-xs text-gray-400 mb-5">Last 60 days</p>
 
           {data.last60Days?.slice(0, 10).map((game) => (
-            <div key={game?.id} className="mb-5 cursor-pointer">
+            <div key={game?.id} className="mb-5">
               <div className="flex gap-4">
                 <div className="flex-shrink-0 w-[100px] h-[100px] relative rounded-lg overflow-hidden group bg-neutral-800">
                   <Image
@@ -36,12 +36,12 @@ export default function ReleaseCalenderComponent({
                     width={100}
                     height={100}
                     alt={game?.name || "Game thumbnail"}
-                    className="object-cover w-full h-full transform transition-transform duration-500 ease-out group-hover:scale-110"
+                    className="cursor-pointer object-cover w-full h-full transform transition-transform duration-500 ease-out group-hover:scale-110"
                   />
                 </div>
                 <div className="mt-1">
-                  <div className="text-sm font-semibold">{game?.name}</div>
-                  <div className="text-sm text-[#9c9c9c]">
+                  <div className="text-sm font-semibold cursor-pointer">{game?.name}</div>
+                  <div className="text-sm text-[#9c9c9c] cursor-pointer">
                     {formatGameReleaseDate(game?.date)}
                   </div>
                 </div>
@@ -56,7 +56,7 @@ export default function ReleaseCalenderComponent({
           <p className="text-xs text-gray-400 mb-5">Next 60 days</p>
 
           {data.next60Days?.slice(0, 10).map((game) => (
-            <div key={game?.id} className="mb-5 cursor-pointer">
+            <div key={game?.id} className="mb-5">
               <div className="flex gap-4">
                 <div className="flex-shrink-0 w-[100px] h-[100px] relative rounded-lg overflow-hidden group bg-neutral-800">
                   <Image
@@ -64,12 +64,12 @@ export default function ReleaseCalenderComponent({
                     width={100}
                     height={100}
                     alt={game?.name || "Game thumbnail"}
-                    className="object-cover w-full h-full transform transition-transform duration-500 ease-out group-hover:scale-110"
+                    className="cursor-pointer object-cover w-full h-full transform transition-transform duration-500 ease-out group-hover:scale-110"
                   />
                 </div>
                 <div className="mt-1">
-                  <div className="text-sm font-semibold">{game?.name}</div>
-                  <div className="text-sm text-[#9c9c9c]">
+                  <div className="text-sm font-semibold cursor-pointer">{game?.name}</div>
+                  <div className="text-sm text-[#9c9c9c] cursor-pointer">
                     {formatGameReleaseDate(game?.date)}
                   </div>
                 </div>
@@ -84,7 +84,7 @@ export default function ReleaseCalenderComponent({
           <p className="text-xs text-gray-400 mb-5">By community hype</p>
 
           {data.mostAnticipated?.slice(0, 10).map((game) => (
-            <div key={game?.id} className="mb-5 cursor-pointer">
+            <div key={game?.id} className="mb-5">
               <div className="flex gap-4">
                 <div className="flex-shrink-0 w-[100px] h-[100px] relative rounded-lg overflow-hidden group bg-neutral-800">
                   <Image
@@ -92,12 +92,12 @@ export default function ReleaseCalenderComponent({
                     width={100}
                     height={100}
                     alt={game?.name || "Game thumbnail"}
-                    className="object-cover w-full h-full transform transition-transform duration-500 ease-out group-hover:scale-110"
+                    className="cursor-pointer object-cover w-full h-full transform transition-transform duration-500 ease-out group-hover:scale-110"
                   />
                 </div>
                 <div className="mt-1">
-                  <div className="text-sm font-semibold">{game?.name}</div>
-                  <div className="text-sm text-[#9c9c9c]">
+                  <div className="text-sm font-semibold cursor-pointer">{game?.name}</div>
+                  <div className="text-sm text-[#9c9c9c] cursor-pointer">
                     {formatGameReleaseDate(game?.date)}
                   </div>
                 </div>
