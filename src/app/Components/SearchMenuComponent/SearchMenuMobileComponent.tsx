7187d93257e13dddebdaa0dae8b82a739d216715
Nibir Alam <nibiralpha@gmail.com>
2026-09-05 19:58:02 +0600

    fix: menu on mobile

--- src/app/Components/SearchMenuComponent/SearchMenuMobileComponent.tsx
+++ src/app/Components/SearchMenuComponent/SearchMenuMobileComponent.tsx
@@ -2,47 +2,72 @@
 
 import React, { useState } from "react";
 import { Drawer } from "vaul";
-import { SearchMenu } from "@app-types/Menu";
+import { platform, genre, feature, Menus } from "@Constant/DataTypes";
+import { SearchMenu } from "../../Types/Menu";
 
 interface MobileFilterDrawerProps {
   children: React.ReactNode;
 }
 
-const menus: SearchMenu[] = [
+const menus: Menus[] = [
   {
     id: 1,
     name: "Platform",
     value: "platform",
     expand: false,
-    childmenus: [{ id: 1, name: "Playstation" }],
+    childMenus: [
+      platform.pc,
+      platform.playStation,
+      platform.xbox,
+      platform.ios,
+      platform.mac,
+      platform.linux,
+      platform.nintendo,
+      platform.android,
+      platform.atari,
+      platform.commodore,
+      platform.sega,
+      platform.threeDfx,
+      platform.neoGeo
+    ]
   },
   {
     id: 2,
     name: "Genre",
     value: "genre",
     expand: false,
-    childmenus: [{ id: 1, name: "Action" }],
+    childMenus: [
+      genre.action,
+      genre.adventure,
+      genre.arcade,
+      genre.boardGames,
+      genre.card,
+      genre.casual,
+      genre.educational,
+      genre.family,
+      genre.fighting,
+      genre.indie,
+      genre.massivelyMultiplier,
+      genre.platformer,
+      genre.puzzle,
+      genre.racing,
+      genre.rpg,
+      genre.shooter,
+      genre.simulation,
+      genre.sports,
+      genre.strategy
+    ]
   },
   {
     id: 4,
     name: "Feature",
     value: "feature",
     expand: false,
-    childmenus: [
-      { id: 1, name: "Multiplayer" },
-      { id: 2, name: "Singleplayer" },
-    ],
-  },
-  // {
-  //   id: 3,
-  //   name: "Release Date",
-  //   value: "release_date",
-  //   expand: false,
-  //   childmenus: [
-  //     { id: 1, name: "2012" },
-  //     { id: 2, name: "2020" },
-  //   ],
-  // },
+    childMenus: [
+      feature.singlePlayer,
+      feature.multiPlayer
+    ]
+  }
 ];
 
 export default function SearchMenuMobileComponent({
@@ -50,11 +75,11 @@ export default function SearchMenuMobileComponent({
 }: MobileFilterDrawerProps) {
   const [menuList, setMenuList] = useState<SearchMenu[]>(menus);
 
-  const toggleMenu = (menuName: string) => {
+  const toggleMenu = (menuId: number) => {
     setMenuList((prev) =>
       prev.map((menu) =>
-        menu.name === menuName ? { ...menu, expand: !menu.expand } : menu,
-      ),
+        menu.id === menuId ? { ...menu, expand: !menu.expand } : menu
+      )
     );
   };
 
@@ -79,56 +104,42 @@ export default function SearchMenuMobileComponent({
                 className="border-b border-[#e1e1e1] last:border-none pb-2 last:pb-0"
               >
                 <div
-                  onClick={() => toggleMenu(menu.name)}
+                  onClick={() => toggleMenu(menu.id)}
                   className="flex justify-between items-center py-3 font-bold cursor-pointer select-none text-black"
                 >
                   <div>{menu.name}</div>
 
-                  {menu.expand === false ? (
-                    <svg
-                      className="w-4 h-4"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      viewBox="0 0 24 24"
-                      xmlns="http://w3.org"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M9 5l7 7-7 7"
-                      ></path>
-                    </svg>
-                  ) : (
-                    <svg
-                      className="w-4 h-4"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      viewBox="0 0 24 24"
-                      xmlns="http://w3.org"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M19 9l-7 7-7-7"
-                      />
-                    </svg>
-                  )}
+                  <svg
+                    className={`w-4 h-4 transform transition-transform duration-200 ${
+                      menu.expand ? "rotate-90" : ""
+                    }`}
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    viewBox="0 0 24 24"
+                    xmlns="http://w3.org"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M9 5l7 7-7 7"
+                    ></path>
+                  </svg>
                 </div>
 
                 {menu.expand && (
                   <div className="pr-2 pb-3 pt-1">
                     <div className="space-y-2.5">
-                      {menu.childmenus?.map((childMenu, index) => (
+                      {menu.childMenus?.map((childMenu) => (
                         <label
                           key={childMenu.id}
                           className="flex w-fit items-center gap-3 cursor-pointer group text-sm font-medium text-[#626262] hover:text-black"
                         >
                           <input
                             type="checkbox"
+                            checked={childMenu.isChecked}
                             onChange={() =>
-                              console.log(`${childMenu}, ${menu.name}`)
+                              console.log(`${childMenu.name}, ${menu.name}`)
                             }
                             className="w-4 h-4 rounded border-gray-300 bg-white cursor-pointer accent-black focus:ring-0"
                           />
